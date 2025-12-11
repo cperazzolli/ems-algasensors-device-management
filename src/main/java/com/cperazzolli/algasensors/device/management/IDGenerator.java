ba2c86f20ea21d6f6a102b15c42783b118cfd617
Claudinei Perazzolli <claudineii.perazzolli@gmail.com>
2025-12-11 20:57:40 -0300

    implementas TSID.

---
+++ src/main/java/com/cperazzolli/algasensors/device/management/IDGenerator.java
@@ -0,0 +1,24 @@
+package com.cperazzolli.algasensors.device.management;
+
+import io.hypersistence.tsid.TSID;
+
+import java.util.Optional;
+
+public class IDGenerator {
+
+    private static final TSID.Factory tsIdFactory;
+
+    static {
+        Optional.ofNullable(System.getenv("tsid.node"))
+                        .ifPresent(tsNode -> System.setProperty("tsid.node", tsNode));
+        Optional.ofNullable(System.getenv("tsid.node.count"))
+                .ifPresent(tsNodeCount -> System.setProperty("tsid.node.count", tsNodeCount));
+        tsIdFactory = TSID.Factory.builder().build();
+    }
+    private IDGenerator() {
+    }
+
+    public static TSID generateId() {
+        return tsIdFactory.generate();
+    }
+}
---
+++ src/test/java/com/cperazzolli/algasensors/device/management/TSIDTest.java
@@ -0,0 +1,25 @@
+package com.cperazzolli.algasensors.device.management;
+
+import io.hypersistence.tsid.TSID;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+
+
+
+class TSIDTest {
+
+    @Test
+    void sholdGenerateTSID() {
+
+        TSID tsid = IDGenerator.generateId();
+
+        Assertions.assertThat(tsid.getInstant()).isCloseTo(Instant.now(), Assertions.within(1, ChronoUnit.MINUTES));
+
+    }
+}
