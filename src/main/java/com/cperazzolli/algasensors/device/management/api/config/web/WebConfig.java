0e200bc620149939c4537321d2092c06bd9bc617
Claudinei Perazzolli <claudineii.perazzolli@gmail.com>
2025-12-12 21:10:16 -0300

    termina de implementar device-manager.

--- src/main/java/com/cperazzolli/algasensors/device/management/api/config/web/WebConfig.java
+++ src/main/java/com/cperazzolli/algasensors/device/management/api/config/web/WebConfig.java
@@ -1,7 +1,5 @@
 package com.cperazzolli.algasensors.device.management.api.config.web;
 
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
-import org.springframework.boot.web.servlet.filter.OrderedFormContentFilter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
